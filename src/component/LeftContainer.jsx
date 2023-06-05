c9e6f5b4b1d20281315cab698d04efea20b4e8cb
Abhishek Jha <abhi10092003@gmail.com>
2023-06-05 18:33:33 +0530

    The posts text can now be correctly accessed. This commit is only for data accessing stage and not for the complete design.

--- src/component/LeftContainer.jsx
+++ src/component/LeftContainer.jsx
@@ -1,13 +1,51 @@
 import '../App.css';
 import React, { useState, useEffect } from 'react';
-import Post from './Post'
+import axios from 'axios';
+import getCookie from './getCookie';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080/',
+});
+
 function LeftContainer(e) {
-	var heading = e.heading;
-	heading = "Posts"
+    const [posts, setPosts] =useState([])
+        var heading = e.heading;
+        heading = "Posts"
+        const student = getCookie()
+        console.log(student)
+        heading=student.user_id+" posts"
+
+        const couldntFetch = (msg)=>{
+            toast.success(msg.data.msg,{
+                position:"bottom-center"
+            });
+        }
+
+        api.post('/fetch_user_post',{
+            user_id:student.user_id
+        },{
+            withCredentials: true
+        }).then(response => {
+            if(response.data.success===1){
+                const newPosts = response.data.data[0].map((post,index)=>(
+                    <div key={index} id='post'>
+                        <h3>{response.data.data[0][index].text}</h3>
+                    </div>
+                ))
+                setPosts(newPosts)
+            }else{
+                couldntFetch(response)
+            }
+        })
+
     return (
         <div id='leftContainer'>
             <h2 id = 'left-heading'>{heading}</h2>
-			<Post/>
+			{posts}
+            <ToastContainer/>
         </div>
     )
 }
