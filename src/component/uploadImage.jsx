ec1b82010d5f5ddf2a56fd745d5c0aab166a743e
Abhishek Jha <abhi10092003@gmail.com>
2024-09-03 15:20:06 +0530

    try 7

--- src/component/uploadImage.jsx
+++ src/component/uploadImage.jsx
@@ -54,31 +54,7 @@ function UploadImage() {
         setImageUrl(downloadURL);
         toast.success('Image uploaded successfully!');
       });
-  })
-
-      uploadTask.on('state_changed',
-        (snapshot) => {
-          const progress =(snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          setUploadProgress(progress);
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused');
-              break;
-            case 'running':
-              console.log('Upload is running');
-              break;
-          }
-        },
-        (error) => {
-          setUploadError(error.message);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setImageUrl(downloadURL);
-            toast.success('Image uploaded successfully!');
-          });
-      });
-    
+  })    
   };
 
   return (
