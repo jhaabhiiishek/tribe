dd3d3e6ef01ac85067f63a3cd34c1c5a7b8451da
Abhishek Jha <abhi10092003@gmail.com>
2024-09-03 15:27:35 +0530

    try 7

--- src/component/uploadImage.jsx
+++ src/component/uploadImage.jsx
@@ -34,14 +34,31 @@ function UploadImage() {
     }
 
     const storageRef = ref(storage, `images/${selectedImage.name}`);// Adjust the path as needed
-    const uploadTask =await uploadBytesResumable(storageRef, selectedImage).then((snapshot) => {
-      const progress =(snapshot.bytesTransferred / snapshot.totalBytes) * 100
-      setUploadProgress(progress);
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        setImageUrl(downloadURL);
-        toast.success('Image uploaded successfully!');
+    const uploadTask = uploadBytesResumable(storageRef, selectedImage);
+
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          const progress =(snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          setUploadProgress(progress);
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        },
+        (error) => {
+          setUploadError(error.message);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUrl(downloadURL);
+            toast.success('Image uploaded successfully!');
+          });
       });
-    })  
+    
   };
 
   return (
