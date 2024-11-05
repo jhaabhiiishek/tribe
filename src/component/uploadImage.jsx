31a58bdccacd2f3ef25fb7c1b4df26c92190d214
Abhishek Jha <abhi10092003@gmail.com>
2024-11-05 19:18:01 +0530

    main

--- src/component/uploadImage.jsx
+++ src/component/uploadImage.jsx
@@ -13,7 +13,7 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket:"tribe-main-proj.appspot.com",
   messagingSenderId: process.env.SENDER_ID,
   appId:process.env.APP_ID ,
   measurementId: process.env.MEASUREMENT_ID
