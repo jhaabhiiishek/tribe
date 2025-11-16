feda3a1dbe29409db147bfb5b064d01dea8c3c5c
Abhishek Jha <abhi10092003@gmail.com>
2025-11-16 15:25:09 +0530

    Loading screen fixed

--- src/component/LoadingAnimation.jsx
+++ src/component/LoadingAnimation.jsx
@@ -1,6 +1,7 @@
 export default function LoadingAnimation() {
   	return (
 		<div className="feature-rotator">
+			<span className="feature-item">Tribe.in</span>
 			<span className="feature-item">Bringing the Images</span>
 			<span className="feature-item">Loading your posts</span>
 			<span className="feature-item">Creating your tribes</span>
